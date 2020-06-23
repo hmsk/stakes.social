38be09b8b2fa3b1879a323a84873aabe5f08e320
hmsk <k.hamasaki@gmail.com>
2020-06-23 02:08:25 -0700

    Give a quick trial of next-i18next

--- packages/web/src/components/organisms/MainHeader/index.tsx
+++ packages/web/src/components/organisms/MainHeader/index.tsx
@@ -4,13 +4,15 @@ import { Button } from 'antd'
 import { Headline } from 'src/components/atoms/Headline'
 import { H1 } from 'src/components/atoms/Typography'
 import { A } from 'src/components/atoms/A'
+import { useTranslation } from 'src/utils/i18n'
 
 export const MainHeader = () => {
+  const { t } = useTranslation()
   return (
     <>
       <Header />
       <Headline>
-        <H1>Make a community sustainable together</H1>
+        <H1>{t('hero')}</H1>
         {A({ href: '/how-it-works' })(
           <Button type="primary" size="large">
             How it works
